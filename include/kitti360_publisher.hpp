4edffe11614933e1c0ffc9f7477317374fbfe423
huiyan <huiyanliu@buaa.edu.cn>
2022-04-21 10:24:41 +0800

    add get and set method

--- include/kitti360_publisher.hpp
+++ include/kitti360_publisher.hpp
@@ -12,6 +12,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
+// this can use abstract class to rebuild structure
 namespace yan{
   /**
    * @brief kitti360 stereo image data loader and publish it to ros
@@ -19,20 +20,44 @@ namespace yan{
    * 
    */
   class Kitti360StereoImagePub{
+  private:
+    ros::NodeHandle& nh_;
+    ros::NodeHandle& pnh_;
+
+    image_transport::Publisher left_pub_;
+    image_transport::Publisher right_pub_;
+    std::string dataset_root_;
+    std::string sequence_;
+  
   public:
     /**
      * @brief Construct a new Kitti360 Stereo Image Pub object
      * 
-     * @param nh  继承的 ros::NodeHandle, 命名空间从node名称开始
-     * @param pnh 私有的 ros::NodeHandle, 命名空间从给定的名字开始
+     * @param nh  name start at "/"
+     * @param pnh name start at "~/"  --->  "/node_name/"
      */
     Kitti360StereoImagePub(ros::NodeHandle &nh, ros::NodeHandle &pnh) 
                 : nh_(nh), pnh_(pnh) {
-      // read param
-      // 读取参数
-      pnh.getParam("dataset_root", dataset_root_);
-      pnh.getParam("sequence", sequence_);
-      ROS_INFO("%s", ("Use dataset : " + dataset_root_ + sequence_).c_str());
+      
+      // initrialize ros publisher of left/righr image
+      // 初始化两个 ros publisher, 对应左右目的图像
+      image_transport::ImageTransport img_tp(pnh);
+      left_pub_ = img_tp.advertise("left_image", 2);
+      right_pub_ = img_tp.advertise("right_image", 2);
+      
+    }
+    /**
+     * @brief Construct a new Kitti360 Stereo Image Pub object
+     * 
+     * @param nh name start at "/"
+     * @param pnh  name start at "~/"  --->  "/node_name/"
+     * @param dataset_root directory of dataset
+     * @param sequence sequence directory of dataset
+     */
+    Kitti360StereoImagePub(ros::NodeHandle &nh, ros::NodeHandle &pnh,
+                           std::string &dataset_root, std::string &sequence) 
+                : nh_(nh), pnh_(pnh),
+                  dataset_root_(dataset_root), sequence_(sequence)  {
       
       // initrialize ros publisher of left/righr image
       // 初始化两个 ros publisher, 对应左右目的图像
@@ -47,12 +72,18 @@ namespace yan{
     /**
      * @brief 发布消息
      * 
-     * @param header 
-     * @param frame_index 
-     * @return true 
-     * @return false 
+     * @param header std_msgs::Header
+     * @param frame_index frame的索引
+     * @return true message pub sucess / 消息发布成功返回true
+     * @return false vice vsersa  / 反之亦然
      */
-    bool image_publish(std_msgs::Header &header, size_t &frame_index){
+    bool publish(std_msgs::Header &header, size_t &frame_index){
+      // dataset directory must be not null
+      // 数据集的路径不能为空
+      if(dataset_root_.empty() || sequence_.empty()){
+        ROS_INFO("Please check your dataset directory or sequence name !");
+        return false;
+      }
       // read data, you can see KITTI360 dataset format in http://www.cvlibs.net/datasets/kitti-360/documentation.php
       // 读取数据, 可以从下面的链接了解 KITTI360 数据集的格式 http://www.cvlibs.net/datasets/kitti-360/documentation.php
       std::stringstream left_img_root_ss, right_img_root_ss, suffix;
@@ -75,13 +106,16 @@ namespace yan{
       return true;
     }
 
-  private:
-    ros::NodeHandle& nh_;
-    ros::NodeHandle& pnh_;
+    void set_data_dir(std::string &dataset_root, std::string &sequence){
+      set_dataset_root(dataset_root);
+      set_sequence(sequence);
+    }
 
-    image_transport::Publisher left_pub_;
-    image_transport::Publisher right_pub_;
-    std::string dataset_root_;
-    std::string sequence_;
+    std::string dataset_root() const { return dataset_root_; }
+    void set_dataset_root(const std::string &dataset_root) { dataset_root_ = dataset_root; }
+
+    std::string sequence() const { return sequence_; }
+    void set_sequence(const std::string &sequence) { sequence_ = sequence; }
+  
   };
 }
--- src/kitti360_helper.cc
+++ src/kitti360_helper.cc
@@ -15,17 +15,20 @@ int main(int argc, char **argv){
   string line;
   size_t frame_idx = 0;
   std::string dataset_root, sequence;
+  // read param
+  // 读取参数
   pnh.getParam("dataset_root", dataset_root);
   pnh.getParam("sequence", sequence);
+  ROS_INFO("%s", ("Use dataset : " + dataset_root + sequence).c_str());
 
-  yan::Kitti360StereoImagePub kitti360_img_pub(nh, pnh);
+  yan::Kitti360StereoImagePub kitti360_img_pub(nh, pnh, dataset_root, sequence);
   while(ros::ok){ // getline(timestamp_file, line) && 
     
     std_msgs::Header header;
     header.stamp = ros::Time::now();
     header.frame_id = "/kitti360_image";
     
-    if(!kitti360_img_pub.image_publish(header, frame_idx)){
+    if(!kitti360_img_pub.publish(header, frame_idx)){
       ROS_INFO("system error : image publish fail.");
     }
 
